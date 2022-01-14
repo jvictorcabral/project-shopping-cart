c5fce4a7fba76a908870085be90d64b166c4cddd
Joao <joaovictorcgjvcg@gmail.com>
2022-01-14 10:59:24 -0400

    Entrega do requisito 9

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,26 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchItem é uma função;', () => {
+    const result = typeof fetchItem === 'function';
+    expect(result).toBe(true);
+  })
+  it('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada;', () => {
+    fetchItem(MLB1615760527);
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527";', () => {
+    fetchItem(MLB1615760527);
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(fetch).toHaveBeenCalled(url);
+  })
+  it('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
+    const fetch = await fetchItem(MLB1615760527);
+    expect(fetch).toEqual(item);
+  })
+  it('Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+    const fetch = await fetchItem();
+    const error = new Error('mensagem esperada aqui');
+    expect(fetch).toEqual(error);
+  })
 });
